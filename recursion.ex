57f7c91748b6f0cc55dff76dba2b15783bf22abf
Aboobacker MK <aboobackervyd@gmail.com>
2016-09-19 21:59:50 +0530

    Indentation

--- recursion.ex
+++ recursion.ex
@@ -1,9 +1,9 @@
-defmodule	Recursion do
-	def print_astrix(n) when n>0 do
-		IO.puts String.duplicate("*",n)
-		print_astrix(n-1)
-	end
-	def print_astrix(0) do
-                IO.puts "-------------"
-        end
+defmodule  Recursion do
+  def print_astrix(n) when n>0 do
+    IO.puts String.duplicate("*",n)
+    print_astrix(n-1)
+  end
+  def print_astrix(0) do
+    IO.puts "-------------"
+  end
 end
